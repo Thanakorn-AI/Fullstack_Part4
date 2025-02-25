cb27344ac8dafd0dc8a62a7bea429289e36062fb
Duke <dukethanakorn@gmail.com>
2025-02-25 21:43:11 +0700

    Completed "4.18: Blog List Expansion, step 6"

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const express = require('express');
 const cors = require('cors');
 const blogRoutes = require('./routes/blogRoutes');
 const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogRoutes);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 module.exports = app;

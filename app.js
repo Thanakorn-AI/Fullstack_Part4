084ed05affef50e0d9710f8b6b3414f821ac9cf4
Thanakorn Kitviriya <dukethanakorn@gmail.com>
2025-02-25 08:55:55 +0700

    Completed "4.15: Blog List Expansion, step 3"

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const blogRoutes = require('./routes/blogRoutes');
+const usersRouter = require('./routes/users');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogRoutes);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,28 @@
+// models/user.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3
+  },
+  name: String,
+  passwordHash: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash; // Don’t expose password hash
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
